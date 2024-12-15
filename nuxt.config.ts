c564e3be5524abcff4330f4ffe3d4538aabdfbb1
Tatsuki-Nishioka <tatsuki130@gmail.com>
2024-12-15 21:27:28 +0900

    generate

--- nuxt.config.ts
+++ nuxt.config.ts
@@ -2,4 +2,12 @@
 export default defineNuxtConfig({
   compatibilityDate: '2024-11-01',
   devtools: { enabled: true },
+  app: {
+    baseURL: '/plant-id-app/'
+  },
+  nitro: {
+    output: {
+      dir: 'docs' // GitHub Pagesのデフォルトディレクトリに合わせて出力ディレクトリを設定
+    }
+  }
 })
